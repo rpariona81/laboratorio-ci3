615592887a49f8e4518dd7e9b6e817c559b509a4
rpariona81 <ronald.pariona.isidro@gmail.com>
2023-08-26 00:29:24 -0500

    setting index database route controller dump-autoload

---
+++ app/controllers/Testcontroller.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class TestController extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->helper('url');
+        $this->load->model('Career_model');
+    }
+
+    public function index()
+    {
+        $data['query'] = $this->Career_model->get_all();
+        $this->load->view('tabletest', $data);
+    }
+}
